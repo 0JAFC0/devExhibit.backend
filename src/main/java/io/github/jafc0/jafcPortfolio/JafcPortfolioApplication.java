a49156643ad3b44e4b40c1e9078130875ab421a4
0jafc0 <jafclive0@gmail.com>
2022-06-30 02:51:33 -0300

    criado arquivos padrões

---
+++ src/main/java/io/github/jafc0/jafcPortfolio/JafcPortfolioApplication.java
@@ -0,0 +1,13 @@
+package io.github.jafc0.jafcPortfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JafcPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JafcPortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/jafc0/jafcPortfolio/JafcPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jafc0.jafcPortfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JafcPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
