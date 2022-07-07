2be52fc3e0571dd14b6abb2775121b13297566b2
0jafc0 <jafclive0@gmail.com>
2022-07-07 18:29:13 -0300

    criado models Experience, TypeEnum, User

---
+++ src/main/java/io/github/jafc0/jafcPortfolio/domain/model/Experience.java
@@ -0,0 +1,46 @@
+package io.github.jafc0.jafcPortfolio.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "experience")
+public class Experience {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name_company")
+    private String nameCompany;
+
+    @Column
+    private String post;
+
+    @Column
+    private String description;
+
+    @Column
+    private String duration;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private TypeEnum type;
+}
---
+++ src/main/java/io/github/jafc0/jafcPortfolio/domain/model/User.java
@@ -0,0 +1,53 @@
+package io.github.jafc0.jafcPortfolio.domain.model;
+
+import java.util.Base64;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String password;
+
+    @Column
+    private Date year;
+
+    @Column
+    private String work;
+
+    @Column(name = "live_in")
+    private String liveIn;
+
+    @Column
+    private Base64 image;
+
+    @Column
+    private String field;
+    
+}
