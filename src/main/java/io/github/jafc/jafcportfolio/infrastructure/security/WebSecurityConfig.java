23f7ef8e6c8c1fcb382c8cd17c6dcfc9f994af60
0jafc0 <jafclive0@gmail.com>
2022-09-08 17:08:53 -0300

    implementado roles para os endpoints

--- src/main/java/io/github/jafc/jafcportfolio/infrastructure/security/WebSecurityConfig.java
+++ src/main/java/io/github/jafc/jafcportfolio/infrastructure/security/WebSecurityConfig.java
@@ -4,14 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import io.github.jafc.jafcportfolio.infrastructure.security.jwt.AuthEntryPointJwt;
@@ -20,12 +21,11 @@ import io.github.jafc.jafcportfolio.infrastructure.security.services.UserDetails
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	private UserDetailsServiceImpl userDetailsServiceImpl;
-	
+
 	@Autowired
 	private AuthEntryPointJwt unauthorizedHandler;
 	
@@ -50,10 +50,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
     	http.authorizeRequests()
-    	.antMatchers("/api/user/signin","/api/user/signup").permitAll()
+    	.expressionHandler(webSecurityExpressionHandler())
+		
     	.antMatchers(SWAGGER_WHITE_LIST).permitAll()
-    	.antMatchers(HttpMethod.GET, "/api/user/**","/api/professional/**","/api/academic/**","/api/project/**","/api/review/**","/api/skill/**").permitAll()
-    	.anyRequest().authenticated()
+		// user configs
+    	.antMatchers("/api/user/signin","/api/user/signup").permitAll()
+    	.antMatchers(HttpMethod.PUT,"/api/user/**").hasRole("ADMIN")
+		.antMatchers(HttpMethod.DELETE,"/api/user/**").hasRole("ADMIN")
+		.antMatchers(HttpMethod.GET,"/api/user/**").hasRole("ADMIN")
+		// ENDPOINTS_WHITELIST configs
+    	.antMatchers(HttpMethod.GET, ENPOINTS_WHITELIST).permitAll()
+    	.antMatchers(HttpMethod.DELETE, ENPOINTS_WHITELIST).hasRole("USER")
+		.antMatchers(HttpMethod.PUT, ENPOINTS_WHITELIST).hasRole("USER")
+		.antMatchers(HttpMethod.POST, ENPOINTS_WHITELIST).hasRole("USER")
+    	.anyRequest().denyAll()
     	.and()
     	.exceptionHandling().authenticationEntryPoint(unauthorizedHandler)
     	.and()
@@ -64,6 +74,28 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     	http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
     
+    @Bean
+    public RoleHierarchyImpl roleHierarchy() {
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
+        return roleHierarchy;
+    }
+    
+    @Bean
+    public DefaultWebSecurityExpressionHandler webSecurityExpressionHandler() {
+        DefaultWebSecurityExpressionHandler expressionHandler = new DefaultWebSecurityExpressionHandler();
+        expressionHandler.setRoleHierarchy(roleHierarchy());
+        return expressionHandler;
+    }
+    
+	private static final String[] ENPOINTS_WHITELIST = {
+		"/api/professional/**",
+		"/api/academic/**",
+		"/api/project/**",
+		"/api/review/**",
+		"/api/skill/**"
+	};
+
     // Constant method with whiteList permissions
     private static final String[] SWAGGER_WHITE_LIST = {
             "/v3/api-docs/**",
