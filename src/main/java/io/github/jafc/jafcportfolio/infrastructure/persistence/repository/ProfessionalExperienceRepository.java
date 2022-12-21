158949dfa2b0491e056fe0f5f6005c4213b676b1
0jafc0 <jafclive0@gmail.com>
2022-12-21 18:11:05 -0300

    adicionado novamente as anotações dos repositorys

--- src/main/java/io/github/jafc/jafcportfolio/infrastructure/persistence/repository/ProfessionalExperienceRepository.java
+++ src/main/java/io/github/jafc/jafcportfolio/infrastructure/persistence/repository/ProfessionalExperienceRepository.java
@@ -6,9 +6,11 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import io.github.jafc.jafcportfolio.domain.model.ProfessionalExperience;
 
+@Repository
 public interface ProfessionalExperienceRepository extends JpaRepository<ProfessionalExperience, Long> {
 	
 	@Query(value = "SELECT * FROM experience e WHERE (e.experience_type = 'ProfessionalExperience') AND (e.user_id = (SELECT id FROM users u WHERE u.email = :email))", nativeQuery = true)
