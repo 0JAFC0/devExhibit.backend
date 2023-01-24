045013a3bdac9d0ec65bbc1684897a5bf1296a50
0jafc0 <jafclive0@gmail.com>
2023-01-24 15:41:11 -0300

    removendo limite do campo image64,começando documentar

--- src/main/java/io/github/jafc/jafcportfolio/presentation/dto/request/ProfessionalExperienceRequest.java
+++ src/main/java/io/github/jafc/jafcportfolio/presentation/dto/request/ProfessionalExperienceRequest.java
@@ -1,5 +1,6 @@
 package io.github.jafc.jafcportfolio.presentation.dto.request;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,15 +12,21 @@ import lombok.Setter;
 @NoArgsConstructor
 public class ProfessionalExperienceRequest {
     
+    @ApiModelProperty(notes = "Id da experiencia profissional salva no banco")
     private Long id;
 
+    @ApiModelProperty(notes = "Nome da area de atuação")
     private String name;
 
+    @ApiModelProperty(notes = "Cargo de atuação")
     private String post;
 
+    @ApiModelProperty(notes = "Descrição da experiencia profissional")
     private String description;
 
+    @ApiModelProperty(notes = "Duração da experiencia profissional em anos")
     private String duration;
 
+    @ApiModelProperty(notes = "Objeto usuario nele vai conter informações como id")
     private UserRequest user;
 }
--- src/main/java/io/github/jafc/jafcportfolio/presentation/dto/request/ReviewRequest.java
+++ src/main/java/io/github/jafc/jafcportfolio/presentation/dto/request/ReviewRequest.java
@@ -1,5 +1,6 @@
 package io.github.jafc.jafcportfolio.presentation.dto.request;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,14 +12,18 @@ import lombok.Setter;
 @NoArgsConstructor
 public class ReviewRequest {
     
+    @ApiModelProperty(notes = "Id do review salvo no banco")
     private Long id;
 
+    @ApiModelProperty(notes = "Titulo do review")
     private String title;
 
+    @ApiModelProperty(notes = "Conteudo do review")
     private String content;
 
+    @ApiModelProperty(notes = "Imagem do review em base64")
     private String imageBase64;
-
+    @ApiModelProperty(notes = "Objeto usuario nele vai conter informações como id")
     private UserRequest user;
 
 }
